30538e0682b8a7219bc2eda8b61b238a9f79a350
bernardo monteiro rocha <mrochabernardo@gmail.com>
2025-01-03 14:45:49 -0300

    Atualização com comentários

--- 01_linguagem_R/02_operadores_matematicos.R
+++ 01_linguagem_R/02_operadores_matematicos.R
@@ -1,38 +1,38 @@
 #  OPERADORES MATEMÁTICOS   #
 
 # Soma
-5 + 3
+5 + 3   # 8
 
 # Subtração
-8 - 6
+8 - 6   # 2
 
 # Multiplicação
-4 * 3
+4 * 3    # 12
 
 # Divisão
-20 / 5
+20 / 5   # 4
 
 # Potenciação
-4 ** 2
+4 ** 2  # 16
 
 # ou
 
-4 ^ 2
+4 ^ 2   # 16
 
 # Radiciação
 
 # raiz quadrada
-sqrt (81)
+sqrt (81)   # 3
 
-81**(1/2)
+81**(1/2)   # 3
 
 # raiz cúbica
-125**(1/3)
-27**(1/3)
+125**(1/3) # 5
+27**(1/3)  # 3
 
 # Módulo
-abs (-10)
-abs (10)
+abs (-10)   # 10
+abs (10)    # 10
 
 # Fatorial
 factorial(3)   # 3.2.1 = 6
@@ -49,8 +49,8 @@ exp(2)
 exp(-2)
 
 # Logatimo natural
-log(2.718281828)
-log(100)
+log(2.718281828)   # 1
+log(100)           # 4.60517
 
 # Constante pi
 pi
@@ -58,27 +58,27 @@ pi
 # pi radianos = 180 graus
 
 # Funçõess trigonométricas (radianos)
-sin(pi/6)  # pi/6 radianos é igual a 30 graus
-asin(0.5)
+sin(pi/6)  # pi/6 radianos é igual a 30 graus   # 0.5 ou 1/2
+asin(0.5)  # 0.5235988
 
-cos(pi/4)  # pi/4 radianos é igual a 45 graus
-acos(0.7071068)
+cos(pi/4)  # pi/4 radianos é igual a 45 graus   # 0.7071068
+acos(0.7071068)                                 # 0.7853981
 
-tan(pi/4)
-atan(1)
+tan(pi/4)  # 1
+atan(1)    # 0.7853982
 
 # Notação científica
 
 2e32 # (2x10^32)
 
-2e3
+2e3 # 2000
 
-2e-26
+2e-26 # 6e+05
 
 2e5 + 4e5
 
-3e20 * 2e12
+3e20 * 2e12   # 6e+05
 
-15e16 / 5e6
+15e16 / 5e6   # 3e+10
 
-getRversion()
+getRversion() # ‘4.3.0’
